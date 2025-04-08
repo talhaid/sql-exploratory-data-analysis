30641dc5cfa02c401677fe4a067022fb832d6810
Talha İsmail Dilek <117217126+talhaid@users.noreply.github.com>
2025-04-08 23:49:01 +0300

    Add files via upload

---
+++ Exploratory Data Analysis.sql
@@ -0,0 +1,87 @@
+-- EDA (Exploratory Data Analysis) Project
+-- In this project, we explore a dataset using SQL to uncover patterns, trends, and interesting insights.
+-- The process involves checking distributions, identifying outliers, and performing grouped aggregations.
+
+--   Tools: MySQL
+
+--   Goals:
+--   • Understand the structure of the data
+--   • Identify patterns and trends
+--   • Prepare hypotheses for further analysis
+
+SELECT *
+FROM layoffs_staging2;
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 2 DESC;
+
+SELECT stage, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+SELECT  SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off) `total`
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `month`
+ORDER BY 1 ASC
+;
+
+
+WITH Rolling_Total AS
+(
+SELECT  SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off) `total_off`
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `month`
+ORDER BY 1 ASC
+)
+SELECT `month`, total_off, SUM(total_off) OVER(ORDER BY `month`) rolling_total
+FROM Rolling_Total;
+
+
+SELECT company, YEAR(`date`) `year`, SUM(total_laid_off) `total_off`
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+
+WITH Company_Year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`) `year`, SUM(total_laid_off) `total_off`
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), Company_Year_Rank AS
+(SELECT *,
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) ranking
+FROM Company_Year
+WHERE years IS  NOT NULL
+)
+SELECT *
+FROM Company_Year_Rank
+WHERE ranking <= 5
+;
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# SQL Exploratory Data Analysis
+
+## 🔍 Project Overview
+This project applies exploratory data analysis (EDA) techniques using SQL. It focuses on discovering trends, patterns, and outliers within the dataset using aggregate functions and groupings.
+
+## 🛠️ Tools & Techniques
+- SQL Queries
+- GROUP BY & ORDER BY
+- JOINs & Subqueries
+- Aggregations: COUNT, AVG, SUM
+
+## 📈 Sample Insights
+- Most active users by transactions
+- Average order value per region
+- Yearly trends in revenue
